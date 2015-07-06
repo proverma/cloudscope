6e37bdce45c5803f2f220bd720571a05b003a134
Pranav Verma <pranav.verma@gmail.com>
2015-07-06 13:42:27 -0700

    fixing mapping to support array mapping

--- cloudscope/xunitMapping.py
+++ cloudscope/xunitMapping.py
@@ -1,53 +1,59 @@
-xunitMapping = { "mappings": {
-                    "testjob": {
+xunitMapping = {"mappings": {
+    "testjob": {
+        "properties": {
+            "duration": {
+                "type": "long"
+            },
+            "id": {
+                "type": "long"
+            },
+            "estimatedDuration": {
+                "type": "long"
+            },
+            "name": {
+                "type": "string"
+            },
+            "result": {
+                "type": "string"
+            },
+            "time": {
+                "type": "date"
+            },
+            "changeSet": {
+                "properties": {
+                    "items": {
                         "properties": {
-                            "duration": {
-                                "type": "long"
-                            },
-                            "id": {
-                                "type": "long"
-                            },
-                            "estimatedDuration": {
-                                "type": "long"
-                            },
-                            "name": {
-                                "type": "string"
-                            },
-                            "result": {
-                                "type": "string"
-                            },
-                            "time": {
-                                "type": "date"
-                            },
-                            "changeSet": {
-                                "type": "object",
-                                "items": {
-                                    "type": "list",
-                                    "author": {
-                                        "type": "object",
-                                        "fullName": {
-                                            "type": "string"
-                                        }
+                            "author": {
+                                "properties": {
+                                    "fullName": {
+                                        "type": "string"
                                     }
-                                }
-                            },
-                            "culprits": {
-                                "type": "list",
-                                "fullName": {
-                                    "type": "string"
                                 }
                             }
                         }
-                    },
-                    "testsuite": {
-                        "_parent": {
-                            "type": "testjob"
-                        }
-                    },
-                    "testcase": {
-                        "_parent": {
-                            "type": "testsuite"
-                        }
+
                     }
+                }
+
+            },
+            "culprits": {
+                "properties" : {
+                    "fullName": {
+                        "type": "string"
+                    }
+                }
+            }
+        }
+    },
+    "testsuite": {
+        "_parent": {
+            "type": "testjob"
+        }
+    },
+    "testcase": {
+        "_parent": {
+            "type": "testsuite"
+        }
+    }
 }
 }
