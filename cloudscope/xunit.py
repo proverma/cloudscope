e6c172b9d0a5d17b461dd4ab2e0f0a46b3e0373b
raghav3259 <raghav.gangal@gmail.com>
2015-07-09 13:32:51 -0700

    fixed some lint issue and added docstring

--- cloudscope/xunit.py
+++ cloudscope/xunit.py
@@ -1,4 +1,5 @@
-"""code that collects logs from jenkins."""
+"""This code collects lthe test logs from jenkins and
+   stroes those test logs into elasticsearch"""
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -12,10 +13,14 @@ es = Elasticsearch()
 
 class XUnitManager(object):
 
-    """main class"""
+    """This is the main class."""
 
     def __init__(self, project, user, password, job_url, last_build_number=0):
-        """defining constructor for getting the credentials."""
+        """constructor, for collecting project credentials.
+           Project: project name, user: your user_name for jenkins
+           password: you password for jenkins, job_url: jenkins job url,
+           last_build_number: testlogs will be collected from the provided
+           build_number"""
         self.project = project
         self.user = user
         self.password = password
@@ -23,36 +28,35 @@ class XUnitManager(object):
         self.last_build_number = last_build_number
         self.es = Elasticsearch()
 
+        # clears cache
         self.es.indices.clear_cache(index=self.project, ignore=400)
+        # puts data into elasticsearch
         requests.put(
             "http://localhost:9200/" + self.project,
             data=json.dumps(xunitMapping))
 
-        # self.es.indices.put_mapping(index=self.project,
-        #                             doc_type=json,
-        #                             body=xunitMapping,
-        #                             ignore=400)
-        # print r.json()
-
     def post_xunit_reports(self):
         """
-        building url, gettting reponse from jenkins
-        and parsing from xml to python dictionary
-        """
+        builds url, collects reponses from the jenkins for the testjob.
+        Parses xml file to dictionary, checks for testcases under testsuites"""
         build_urls = self.get_new_builds(self.job_url, self.last_build_number)
-        # print build_urls
+
         for k in build_urls:
             response_json = self.call_jenkins(k + '/api/json')
             self.index_test_job(k, response_json["fullDisplayName"],
-                                response_json["id"], response_json["timestamp"],
-                                response_json["result"], response_json["duration"],
-                                response_json["estimatedDuration"], response_json["changeSet"],
+                                response_json["id"],
+                                response_json["timestamp"],
+                                response_json["result"],
+                                response_json["duration"],
+                                response_json["estimatedDuration"],
+                                response_json["changeSet"],
                                 response_json["culprits"])
             urls = self.get_xunit_report_urls(k, response_json["artifacts"])
             for u in urls:
                 self.testsuite_counter = 0
                 report = xmltodict.parse(
-                    requests.get(u, auth=HTTPBasicAuth(self.user, self.password)).content)
+                    requests.get(u, auth=HTTPBasicAuth(self.user,
+                                                       self.password)).content)
                 testSuite = report["testsuites"]["testsuite"]
                 print u
                 # TODO : fix duplicate code
@@ -72,11 +76,9 @@ class XUnitManager(object):
 
     def index_testcase(self, testcases, testsuite_id):
         """
-        checking the number of test cases
-        and displaying the testcases details
+        checks for the testcases, if a suite has only when testcase
+        then returns an object instead of dictionary.
         """
-        # if a suite has just one testcase, cml2dict returns an object instead
-        # of dict, handling that scenario
         self.testcase_counter = 0
         # TODO : fix duplicate code
         if isinstance(testcases, list):
@@ -91,14 +93,19 @@ class XUnitManager(object):
             testcase_id = testsuite_id + "/testcase/" + \
                 str(self.testcase_counter)
             self.es.index(index=self.project, id=testcase_id,
-                          doc_type="testcase", body=testcases, parent=testsuite_id)
+                          doc_type="testcase", body=testcases,
+                          parent=testsuite_id)
 
-    def index_test_job(self, url, name, id, time, result, duration, estimatedDuration,
-                       changeSet, culprits):
-        """defining testjob index"""
+    def index_test_job(self, url, name, id, time, result, duration,
+                       estimatedDuration, changeSet, culprits):
+        """defines testjob index which includes: testjob name, id,
+           timestamp: when the test was run, test execution duration,
+           estimatedDuration, changeSet: person who created the build,
+           culprits: person who contributed in the build."""
 
-        # author_list = [item['author']['fullName'] for item in changeSet['items']]
-        # culprit_list = [each['fullName'] for each in culprits]
+        author_list = [item['author']['fullName']
+                       for item in changeSet['items']]
+        culprit_list = [each['fullName'] for each in culprits]
         testJob = {
             "name": name,
             "id": int(id),
@@ -106,15 +113,16 @@ class XUnitManager(object):
             strftime('%Y-%m-%dT%H:%M:%S'), "result": result,
             "duration": duration,
             "estimatedDuration": estimatedDuration,
-            "changeSet": [item['author']['fullName'] for item in changeSet['items']],
-            "culprits": [each['fullName'] for each in culprits]
+            "changeSet": author_list,
+            "culprits": culprit_list
         }
 
         self.es.index(
             index=self.project, doc_type="testjob", id=url, body=testJob)
 
     def index_test_suite(self, testsuite, build_url):
-        """defining testsuit index"""
+        """creates the index for testsuites and checks
+           for the testcase in testsuites."""
         try:
             # testSuite.pop("testcase")
             self.testsuite_counter += 1
@@ -122,7 +130,8 @@ class XUnitManager(object):
                 str(self.testsuite_counter)
 
             self.es.index(index=self.project, id=testsuite_id,
-                          doc_type="testsuite", body=testsuite, parent=build_url)
+                          doc_type="testsuite", body=testsuite,
+                          parent=build_url)
             return testsuite_id
         except KeyError:
             print "No Testcase found"
@@ -131,14 +140,15 @@ class XUnitManager(object):
 
     def call_jenkins(self, url):
         """
-        calling jenkins by passing credentials
+        connects to jenkins by passing jenkin job url, username and password
         """
         r = requests.get(url, auth=HTTPBasicAuth(self.user, self.password))
         return r.json()
 
     def get_new_builds(self, job_url, last_build_number):
         """
-        getting new bulds from jenkins
+        checks for the new builds in jenkins and append
+        the url whenever there is new build.
         """
         builds = []
         response_json = self.call_jenkins(job_url + 'api/json')
@@ -150,8 +160,8 @@ class XUnitManager(object):
 
     def get_xunit_report_urls(self, job_url, artifacts):
         """
-        checking for the xml and appending url with artifacts
-        and relativepath
+        checks for the artifacts and append url with the
+        artifacts and xml files.
         """
         urls = []
 
